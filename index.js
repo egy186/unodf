925a2650fe83d98684c0667d6f170312cef1a29e
egy186 <egy186@yahoo.co.jp>
2016-02-29 18:55:13 +0900

    add bin

--- index.js
+++ index.js
@@ -2,31 +2,24 @@
 
 const AdmZip = require('adm-zip');
 const beautifyHtml = require('js-beautify').html;
-const fs = require('fs');
-const mkdirp = require('mkdirp');
 const path = require('path');
 
-const unODF = (filePath, outDir, pretty) => {
-  const folder = path.join(outDir || '', path.basename(filePath, filePath.slice(filePath.lastIndexOf('.'))));
-  const zip = new AdmZip(filePath);
+const unODF = (input, pretty) => {
+  const zip = new AdmZip(input);
   const zipEntries = zip.getEntries();
 
-  mkdirp.sync(folder);
-
-  zipEntries.forEach(zipEntry => {
+  return zipEntries.reduce((obj, zipEntry) => {
     const entryName = zipEntry.entryName;
-    if (entryName.includes('/')) {
-      mkdirp.sync(path.join(folder, entryName.slice(0, entryName.lastIndexOf('/'))));
-    }
-    if (entryName.slice(entryName.lastIndexOf('.') + 1).toLowerCase() === 'xml' && pretty !== false) {
-      fs.writeFileSync(path.join(folder, entryName), beautifyHtml(zipEntry.getData().toString('utf8'), {
+    if (path.extname(entryName).toLowerCase() === '.xml' && pretty) {
+      obj[entryName] = new Buffer(beautifyHtml(zipEntry.getData().toString('utf8'), {
         indent_size: 2,
         end_with_newline: true
       }));
     } else {
-      fs.writeFileSync(path.join(folder, entryName), zipEntry.getData());
+      obj[entryName] = zipEntry.getData();
     }
-  });
+    return obj;
+  }, {});
 };
 
 module.exports = unODF;
